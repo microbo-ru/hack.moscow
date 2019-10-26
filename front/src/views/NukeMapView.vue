148fbacc7e28a5fcad92b751eb900736ceee1019
ivanblinov2k17 <ivanblinov2015@mail.ru>
2019-10-26 12:07:15 +0300

    working non-interactive map

---
+++ front/src/views/NukeMapView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <HereMap
+      appId="lmDfff27MoZlUJmc5ZgX"
+      appCode="98vUqPDEgoMgriX_YWU4wA"
+      lat="37.7397"
+      lng="-121.4252"
+      width="100%"
+      height="835px"
+    />
+  </div>
+</template>
+
+<script>
+import HereMap from "../components/NukeMap.vue";
+
+export default {
+  name: "NukeMapView",
+  components: {
+    HereMap
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
