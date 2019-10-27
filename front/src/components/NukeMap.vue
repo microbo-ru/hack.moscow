ac9f6e0b001e91482df6cbf437ac022e445f87cb
ivanblinov2k17 <ivanblinov2015@mail.ru>
2019-10-27 05:45:48 +0300

    circlesss

--- front/src/components/NukeMap.vue
+++ front/src/components/NukeMap.vue
@@ -5,33 +5,34 @@
 </template>
 
 <script>
-function getCoordByName(name) {
-  return fetch(
-    `https://geocode-maps.yandex.ru/1.x/?apikey=2d121788-cbff-41a5-9cfb-62e349951e8b&format=json&geocode=${name}`
-  ).then(response => {
-    if (response.ok) {
-      return response.json().then(json => {
-        console.log(json);
-        return json.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
-          .split(" ")
-          .map(parseFloat);
-      });
-    }
-  });
-}
+// function getCoordByName(name) {
+//   return fetch(
+//     `https://geocode-maps.yandex.ru/1.x/?apikey=2d121788-cbff-41a5-9cfb-62e349951e8b&format=json&geocode=${name}`
+//   ).then(response => {
+//     if (response.ok) {
+//       return response.json().then(json => {
+//         console.log(json);
+//         return json.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos
+//           .split(" ")
+//           .map(parseFloat);
+//       });
+//     }
+//   });
+// }
 
-function addCircleMarker(name, map, behavior, H) {
-  getCoordByName(name).then(function(res) {
-    console.log(res);
-    var circle = new H.map.Circle({ lat: res[1], lng: res[0] }, 8000);
-    map.addObject(circle);
-  });
-}
+// function addCircleMarker(name, map, behavior, H) {
+//   getCoordByName(name).then(function(res) {
+//     console.log(res);
+//     var circle = new H.map.Circle({ lat: res[1], lng: res[0] }, 8000);
+//     map.addObject(circle);
+//   });
+// }
 
 export default {
   name: "HereMap",
   data() {
     return {
+      persons: {},
       map: {},
       platform: {}
     };
@@ -50,6 +51,48 @@ export default {
       app_id: this.appId,
       app_code: this.appCode
     });
+  },
+  methods: {
+    async fetch_data(map, behavior, H) {
+      this.persons = await fetch("http://localhost:5000/api/map", {
+        "Content-type": "application/json"
+      }).then(res => {
+        res.json().then(res => {
+          let sqa = res[0]["years"][2016]["total_sq"];
+          let sqb = res[1]["years"][2018]["total_sq"];
+          let sqfama = 52072785.99999999;
+          console.log(sqa, sqb);
+          let ra = Math.sqrt(sqa / Math.PI);
+          let rb = Math.sqrt(sqb / Math.PI);
+          let rfama = Math.sqrt(sqfama / Math.PI);
+          console.log(ra, rb);
+          var circle = new H.map.Circle({ lat: 59.944844, lng: 30.246036 }, ra);
+          map.addObject(circle);
+          var circle2 = new H.map.Circle(
+            { lat: 59.924138, lng: 30.351958 },
+            rb,
+            {
+              style: {
+                fillColor: "rgba(0, 255, 24, 0.2)" // Color of the circle
+              }
+            }
+          );
+          map.addObject(circle2);
+          var circle3 = new H.map.Circle(
+            { lat: 59.892215, lng: 30.321622 },
+            rfama,
+            {
+              style: {
+                fillColor: "rgba(250, 255, 24, 0.2)" // Color of the circle
+              }
+            }
+          );
+          map.addObject(circle3);
+          console.log(sqa, sqb);
+        });
+        // return res.json();
+      });
+    }
   },
   mounted() {
     var H = window.H;
@@ -71,8 +114,8 @@ export default {
           "png"
         ),
       {
-        center: new H.geo.Point(60, 51),
-        zoom: 4,
+        center: new H.geo.Point(59.928761, 30.310878),
+        zoom: 11,
         style: "default",
         pixelRatio: pixelRatio
       }
@@ -85,8 +128,8 @@ export default {
     ui.removeControl("mapsettings");
 
     var map = this.map;
-    getCoordByName("Саратов").then(console.log);
-    addCircleMarker("Санкт-Петербург", map, behavior, H);
+    // addCircleMarker("Санкт-Петербург", map, behavior, H);
+    this.fetch_data(map, behavior, H);
   }
 };
 </script>
